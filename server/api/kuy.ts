6cfdd082bcfa9df410cf5a7465bb1759b2862d74
mina <peemlomsak@gmail.com>
2023-04-26 23:52:24 +0700

    test3

--- server/api/kuy.ts
+++ server/api/kuy.ts
@@ -1,11 +1,36 @@
-export default defineEventHandler(event => {
-    const cityHeader = getHeader(event, 'x-vercel-ip-city')
-    const city = cityHeader ? decodeURIComponent(cityHeader) : '-'
-    const ipHeader = getHeader(event, 'x-forwarded-for')
-    const ip = ipHeader ? ipHeader.split(',')[0] : '-'
-  
-    return {
-      city,
-      ip
-    }
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    organization: "org-XPLjH6Gx2RpnHEf4Ajg7N2Hj",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export const config = {
+  runtime: 'edge',
+  regions: 'iad1', // optional
+};
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event)
+  const previousMessages: ChatCompletionRequestMessage[] = body.messages;
+
+  if (previousMessages) {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {role: "system", content: `You are a cute Japanese assistant who talks in all lowercase, doesn't use punctuation and Your name is Akiko and response must be under 150 words and respond in Japanese`},
+        {role: "assistant", content: 'こんにちは、どうすればお手伝いできますか'},
+        ...previousMessages
+      ],
+      temperature: 0,
+      max_tokens: 200
+    });
+    
+    return completion.data.choices[0].message?.content
+  }
+
+  throw createError({
+    statusCode: 400,
+    statusMessage: 'Input must be provided',
+  })
 })
