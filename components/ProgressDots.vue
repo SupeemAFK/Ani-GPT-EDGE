dc75e42fd12b949bf2cc480790236caa347e887e
mina <peemlomsak@gmail.com>
2023-04-27 19:57:37 +0700

    add components and improve styles

---
+++ components/ProgressDots.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="relative flex items-center justify-center gap-1 bg-slate-300 p-2 px-3 rounded-md">
+        <div class="bounce h-2 w-2 animate-three-bounce rounded-full bg-slate-500"></div>
+        <div class="bounce h-2 w-2 animate-three-bounce rounded-full bg-slate-500"></div>
+        <div class="bounce h-2 w-2 animate-three-bounce rounded-full bg-slate-500"></div>
+    </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Poppins', 'sans-serif']
+      },
+      keyframes: {
+        // three bounce
+        'three-bounce': {
+          '0%, 100%': {
+            transform: 'scale(0.5)',
+            opacity: '0.5',
+          },
+          '50%': {
+            transform: 'scale(1.25)',
+            opacity: '1',
+          },
+        },
+      },
+      animation: {
+        //three-bounce
+        'three-bounce': 'three-bounce 1s ease-in-out infinite',
       }
     },
   },
